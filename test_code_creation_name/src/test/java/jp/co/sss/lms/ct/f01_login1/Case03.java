a80cb7d796f33827efc5db0b9ab3cd93539d6d14
youzhijiesanqiao28 <youzhijiesanqiao28@gmail.com>
2025-11-27 16:30:32 +0900

    fix  :RP2_10_結合テスト仕様書_1.1.xls ケース03,04の記述
    add
     :Case03.java　ログインテストコード追加
    　　正しいIDとパスワードの値を基にログインできるか確認するテストを作成した。
    
    　　期待値は以下の通り
    　　「http://localhost:8080/lms/course/detail」へ遷移
    
     :Case04.java　よくある質問画面へ遷移するテストコードの追加
    
    　　下記の動作を行い、よくある質問画面に遷移するできるか確認するテストを作成した。
    　　・メニューバーの機能メニューから「ヘルプ」リンクを押下
    　　・http://localhost:8080/lms/help内にある「よくある質問」リンクを押下
    
    　　期待値は以下の通り
    　　「http://localhost:8080/lms/faq」へ遷移

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,13 +38,43 @@ public class Case03 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//画面遷移
+		goTo("http://localhost:8080/lms");
+
+		// ページタイトルからログイン画面であることを検証
+		String checkTitle = webDriver.getTitle();
+		assertEquals("ログイン | LMS", checkTitle);
+
+		// エビデンスの保存
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//ログインIDの表示を待機
+		visibilityTimeout(By.name("loginId"), 10);
+
+		// 存在しないログインID・パスワードを入力
+		webDriver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.name("password")).sendKeys("uP61MpS4");
+
+		// ログインボタン押下
+		webDriver.findElement(By.cssSelector("input[value='ログイン']")).click();
+
+		// ログイン成功画面の見出しが「DEMOコース」であるか判定
+		String actualCourseName = webDriver.findElement(
+				By.cssSelector("h2.di")).getText();
+
+		assertTrue(actualCourseName.startsWith("DEMOコース"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -36,27 +38,112 @@ public class Case04 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+
+		// ページタイトルからログイン画面であることを検証
+		String checkTitle = webDriver.getTitle();
+		assertEquals("ログイン | LMS", checkTitle);
+
+		// エビデンスの保存
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインIDの表示を待機
+		visibilityTimeout(By.name("loginId"), 10);
+
+		// 存在しないログインID・パスワードを入力
+		webDriver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.name("password")).sendKeys("uP61MpS4");
+
+		// ログインボタン押下
+		webDriver.findElement(By.cssSelector("input[value='ログイン']")).click();
+
+		String actualCourseName = webDriver.findElement(
+				By.cssSelector("h2.di")).getText();
+
+		assertTrue(actualCourseName.startsWith("DEMOコース"));
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
-	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
+	@DisplayName("テスト03 機能メニューの『ヘルプ』からヘルプ画面に遷移し表示を確認")
 	void test03() {
-		// TODO ここに追加
+
+		// 機能メニューのドロップダウンを開くまで待つ
+		visibilityTimeout(By.linkText("機能"), 10);
+
+		// 「機能」をクリックしてドロップダウン表示
+		webDriver.findElement(By.linkText("機能")).click();
+
+		// 「ヘルプ」が表示されるまで待機
+		visibilityTimeout(By.linkText("ヘルプ"), 10);
+
+		// 「ヘルプ」をクリック
+		webDriver.findElement(By.linkText("ヘルプ")).click();
+
+		// ページロード待ち
+		pageLoadTimeout(5);
+
+		// 現在のURL取得
+		String currentUrl = webDriver.getCurrentUrl();
+
+		// URL が /lms/help であることを確認
+		//assertEquals("http://localhost:8080/lms/help", currentUrl);
+
+		// コンソールへ出力
+		//System.out.println("現在のURL: " + currentUrl);
+
+		// h2 タグの文言が「ヘルプ」であることを確認
+		String title = webDriver.findElement(By.tagName("h2")).getText();
+		assertTrue(title.startsWith("ヘルプ"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+
+		//リンクが表示されるまで待機
+		visibilityTimeout(By.linkText("よくある質問"), 10);
+
+		//現在のタブを保存
+		//String originalTab = webDriver.getWindowHandle();
+
+		//新規タブで開く
+		webDriver.findElement(By.linkText("よくある質問")).click();
+
+		//新しいタブ一覧を取得
+		java.util.List<String> tabs = new java.util.ArrayList<>(webDriver.getWindowHandles());
+
+		// 新しいタブへ切り替え
+		webDriver.switchTo().window(tabs.get(1));
+
+		// 6. URL確認
+		String currentUrl = webDriver.getCurrentUrl();
+		System.out.println("【現在のURL】" + currentUrl);
+		assertEquals("http://localhost:8080/lms/faq", currentUrl);
+
+		// 7. h2の確認（ここでページ読み込みが完了しSelenium実行コンテキストも安定する）
+		visibilityTimeout(By.tagName("h2"), 10);
+		String h2Text = webDriver.findElement(By.tagName("h2")).getText();
+		assertEquals("よくある質問", h2Text);
+
+		//System.out.println("取得したh2: " + h2Text);
+
+		getEvidence(new Object() {
+		});
 	}
 
 }
